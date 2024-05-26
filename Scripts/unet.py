be80f8f986ab6a19eceed738b7830544122d3629
Shay Levi <shay2301@gmail.com>
2024-05-26 23:01:35 +0300

    Fixing the unet to have sigmoid as an output so our model would give proper values in predicition in the range of (0,1)

--- Scripts/unet.py
+++ Scripts/unet.py
@@ -123,7 +123,7 @@ class OutConv(nn.Module):
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
 
     def forward(self, x):
-        return self.conv(x)
+        return torch.sigmoid(self.conv(x))
 
 class UNet(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=True):
@@ -159,4 +159,4 @@ class UNet(nn.Module):
         return logits
     
     def predict(self, x):
-        return F.softmax(self.forward(x), dim=1)
+        return torch.sigmoid(self.forward(x))
