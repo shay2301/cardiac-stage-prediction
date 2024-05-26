778d76960180ad86efe5e6bb4f9da600d366e752
Shay Levi <shay2301@gmail.com>
2024-05-26 23:54:03 +0300

    Modified the script in a way that prevent the computer to hibernate while training the network

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import ctypes
 import pandas as pd
 from torch import nn
 from torch import save, load
@@ -9,6 +10,20 @@ from Scripts import unet
 from Scripts import dataloader
 from Scripts.dataloader import imshow
 import os
+
+# Constants for power settings
+ES_CONTINUOUS = 0x80000000
+ES_SYSTEM_REQUIRED = 0x00000001
+
+# Function to prevent sleep
+def prevent_sleep():
+    ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED)
+
+# Function to allow sleep
+def allow_sleep():
+    ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS)
+
+prevent_sleep()
 
 root_dir = os.path.dirname(__file__)
 csv_full_path = os.path.join(root_dir, 'Database', 'training_database.csv')
@@ -36,3 +51,5 @@ criterion = unet.DiceLoss()
 trained_model = unet.train(model, optimizer, criterion, train_dataloader, val_dataloader, epochs=6)
 
 save(trained_model.state_dict(), os.path.join(root_dir, 'models', 'model_dice_6epochs_augmented.pth'))
+
+allow_sleep()
