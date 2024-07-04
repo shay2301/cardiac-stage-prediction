ca1d80379411a1c89635919e0b2a43eae592c933
Shay Levi <shay2301@gmail.com>
2024-07-04 09:46:00 +0300

    data loading

--- Scripts/data_transforms.py
+++ Scripts/data_transforms.py
@@ -1,6 +1,59 @@
 from torchvision import transforms
+import numpy as np
 import torch
 import random
+from torchvision.transforms import functional as F
+from PIL import Image
+import matplotlib.pyplot as plt
+
+# def normalize_image(image):
+#     image = image.astype(np.float32)
+#     image = (image - np.min(image)) / (np.max(image) - np.min(image))
+#     return image
+
+class RandomAddNoise:
+    def __init__(self, mean=0.0, std=0.01, p=0.5):
+        self.mean = mean
+        self.std = std
+        self.p = p
+
+    def __call__(self, img):
+        if random.random() < self.p:
+            if isinstance(img, np.ndarray):
+                img = self.normalize_image(img)
+                noise = (np.random.randn(*img.shape) * self.std + self.mean).astype(np.float32)
+                noisy_img = img.astype(np.float32) + noise
+                noisy_img = self.normalize_image(noisy_img)
+                return Image.fromarray((noisy_img * 255).astype(np.uint8))
+            elif isinstance(img, torch.Tensor):
+                noise = torch.randn(img.size(), dtype=torch.float32) * self.std + self.mean
+                noisy_img = img + noise
+                noisy_img = torch.clamp(noisy_img, 0.0, 1.0)
+                return transforms.ToPILImage()(noisy_img)
+            elif isinstance(img, Image.Image):
+                img_array = np.array(img).astype(np.float32) / 255.0
+                img_array = self.normalize_image(img_array)
+                noise = (np.random.randn(*img_array.shape) * self.std + self.mean).astype(np.float32)
+                noisy_img_array = img_array + noise
+                noisy_img_array = self.normalize_image(noisy_img_array)
+                return Image.fromarray((noisy_img_array * 255).astype(np.uint8))
+            else:
+                raise TypeError(f"Unsupported image type: {type(img)}")
+        else:
+            if isinstance(img, np.ndarray):
+                return Image.fromarray(img.astype(np.uint8))
+            elif isinstance(img, torch.Tensor):
+                return transforms.ToPILImage()(img)
+            elif isinstance(img, Image.Image):
+                return img
+            else:
+                raise TypeError(f"Unsupported image type: {type(img)}")
+            
+    @staticmethod
+    def normalize_image(img):
+        min_val = np.min(img)
+        max_val = np.max(img)
+        return (img - min_val) / (max_val - min_val + 1e-5)
 
 class SameSeed:
     def __init__(self, seed=42):
@@ -10,36 +63,141 @@ class SameSeed:
         torch.manual_seed(self.seed)
         return img
 
-class RandomChoiceTransform:
-    def __init__(self, transforms, seed=42):
-        self.transforms = transforms
-        self.seed = seed
-
-    def __call__(self, img):
-        random.seed(self.seed)
-        transform = random.choice(self.transforms)
-        return transform(img)
-    
-input_transform = transforms.Compose([
-    SameSeed(),
-    RandomChoiceTransform([
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation((0, 360)),
-    ], seed=42),
+base_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    # RandomAddNoise(mean=0.0, std=0.1),
     transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
 ])
 
-target_transform = transforms.Compose([
-    SameSeed(),
-    RandomChoiceTransform([
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation((0, 360)),
-    ], seed=42),
+horizontal_flip_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.hflip(x),
+    transforms.ToTensor(),
+])
+
+vertical_flip_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.vflip(x),
+    transforms.ToTensor(),
+])
+
+rotate_15_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 15),
+    transforms.ToTensor(),
+])
+
+rotate_30_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 30),
+    transforms.ToTensor(),
+])
+
+rotate_45_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 45),
+    transforms.ToTensor(),
+])
+
+rotate_60_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 60),
+    transforms.ToTensor(),
+])
+
+rotate_75_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 75),
+    transforms.ToTensor(),
+])
+
+
+rotate_90_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 90),
+    transforms.ToTensor(),
+])
+
+rotate_105_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 105),
+    transforms.ToTensor(),
+])
+
+rotate_120_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 120),
+    transforms.ToTensor(),
+])
+
+rotate_135_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 135),
+    transforms.ToTensor(),
+])
+
+rotate_150_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 150),
+    transforms.ToTensor(),
+])
+
+rotate_165_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 165),
+    transforms.ToTensor(),
+])
+
+rotate_180_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 180),
+    transforms.ToTensor(),
+])
+
+rotate_195_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 195),
+    transforms.ToTensor(),
+])
+
+rotate_210_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 210),
+    transforms.ToTensor(),
+])
+
+rotate_225_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 225),
+    transforms.ToTensor(),
+])
+
+rotate_240_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 240),
+    transforms.ToTensor(),
+])
+
+rotate_255_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 255),
+    transforms.ToTensor(),
+])
+
+rotate_270_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    lambda x: F.rotate(x, 270),
+    transforms.ToTensor(),
+])
+
+
+noise_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    RandomAddNoise(mean=0.0, std=0.1, p=1.0),
     transforms.ToTensor(),
 ])
 
 video_transform = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
+    transforms.Normalize((0.1283,), (0.1901,))
 ])
--- Scripts/dataloader.py
+++ Scripts/dataloader.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import torch
 from torch.utils.data import Dataset, DataLoader
+from Scripts.data_transforms import base_transform
 from torchvision import transforms
 import os
 
@@ -16,7 +17,7 @@ def convert_to_list(input_string):
     number_list = [list(map(int, match)) for match in matches]
     return number_list
 
-def imshow(batch, esv_edv_pred_imgs=None, alpha=0.5):
+def imshow(batch, esv_edv_pred_imgs=None, alpha=0.5, threshold=0.5):
     esv_imgs = batch['frames']['esv']
     edv_imgs = batch['frames']['edv']
     esv_masks = batch['masks']['esv']
@@ -39,11 +40,11 @@ def imshow(batch, esv_edv_pred_imgs=None, alpha=0.5):
         esv_pred_rgba_masks = [np.zeros((mask.shape[1], mask.shape[2], 4), dtype=np.float32) for mask in esv_masks]
         edv_pred_rgba_masks = [np.zeros((mask.shape[1], mask.shape[2], 4), dtype=np.float32) for mask in edv_masks]
         for i, (esv_pred_img, edv_pred_img) in enumerate(zip(esv_pred_imgs, edv_pred_imgs)):
-            esv_pred_rgba_masks[i][esv_pred_img.squeeze() > 0.5] = [0, 1, 0, alpha]
-            edv_pred_rgba_masks[i][edv_pred_img.squeeze() > 0.5] = [0, 1, 0, alpha]
+            esv_pred_rgba_masks[i][esv_pred_img.squeeze() > threshold] = [0, 1, 0, alpha]
+            edv_pred_rgba_masks[i][edv_pred_img.squeeze() > threshold] = [0, 1, 0, alpha]
 
-            esv_pred_rgba_masks[i][esv_pred_img.squeeze() <= 0.5] = [0, 0, 0, 0]
-            edv_pred_rgba_masks[i][edv_pred_img.squeeze() <= 0.5] = [0, 0, 0, 0]
+            esv_pred_rgba_masks[i][esv_pred_img.squeeze() <= threshold] = [0, 0, 0, 0]
+            edv_pred_rgba_masks[i][edv_pred_img.squeeze() <= threshold] = [0, 0, 0, 0]
 
     fig, axes = plt.subplots(2, len(esv_imgs), figsize=(20, 20))
     plt.subplots_adjust(wspace=0, hspace=-0.5)
@@ -81,19 +82,20 @@ class Patient():
         self.split = None
 
 class VideoFrameDataset(Dataset):
-    def __init__(self, root_dir, input_transform=None, target_transform=None,split_group=None, split_method=None, test_size=0.2, val_size=0.2):
+    def __init__(self, root_dir, transform=None, noise_flag=False, split_group=None, split_method=None, test_size=0.2, val_size=0.2, resize_flag=False):
         self.root_dir = root_dir
         csv_file = os.path.join(root_dir, 'Database', 'training_database.csv')
         self.df = pd.read_csv(csv_file)
         self.df_train = None
         self.df_val = None
         self.df_test = None
-        self.input_transform = input_transform
-        self.target_transform = target_transform
+        self.transform = transform
+        self.noise_flag = noise_flag
         self.split_group = split_group
         self.split_method = split_method
         self.test_size = test_size
         self.val_size = val_size
+        self.resize_flag = resize_flag
 
         if not self.split_method:
             self.df_train = self.df[self.df['Split'].str.lower() == 'train']
@@ -130,48 +132,63 @@ class VideoFrameDataset(Dataset):
             cap.set(cv2.CAP_PROP_POS_FRAMES, index)
             ret, frame = cap.read()
             if ret:
-                if self.input_transform:
+
+                gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                if self.resize_flag:
+                    gray_frame = cv2.resize(gray_frame, (224, 224))  # Resize frame to 224x224
+                if self.transform:
                     if patient.esv_idx == index:
-                        patient.esv_frame = self.input_transform(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+                        patient.esv_frame = self.transform(gray_frame)
+                        patient.esv_frame = transforms.Normalize((0.1283,), (0.1901,))(patient.esv_frame)
                     elif patient.edv_idx == index:
-                        patient.edv_frame = self.input_transform(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+                        patient.edv_frame = self.transform(gray_frame)
+                        patient.edv_frame = transforms.Normalize((0.1283,), (0.1901,))(patient.edv_frame)
                 else:
                     if patient.esv_idx == index:
-                        patient.esv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                        patient.esv_frame = gray_frame
                     elif patient.edv_idx == index:
-                        patient.edv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                        patient.edv_frame = gray_frame
 
         cap.release()
 
-        channels, height, width = patient.esv_frame.shape
-
+        height, width, channels = frame.shape
 
         patient.esv_mask = np.zeros((height, width), dtype=np.uint8)
         patient.edv_mask = np.zeros((height, width), dtype=np.uint8)
 
-        
         cv2.fillPoly(patient.esv_mask, [np.array(convert_to_list(patient.esv_poly))], 255)
         cv2.fillPoly(patient.edv_mask, [np.array(convert_to_list(patient.edv_poly))], 255)
+
+        if self.resize_flag:
+            patient.esv_mask = cv2.resize(patient.esv_mask, (224, 224), interpolation=cv2.INTER_NEAREST)
+            patient.edv_mask = cv2.resize(patient.edv_mask, (224, 224), interpolation=cv2.INTER_NEAREST)
 
         patient.esv_mask = np.expand_dims(patient.esv_mask, axis=2)
         patient.edv_mask = np.expand_dims(patient.edv_mask, axis=2)
 
-        if self.target_transform:
-            patient.esv_mask = self.target_transform(patient.esv_mask)
-            patient.edv_mask = self.target_transform(patient.edv_mask)
+        if self.transform:
+            if self.noise_flag:
+                patient.esv_mask = base_transform(patient.esv_mask)
+                patient.edv_mask = base_transform(patient.edv_mask)
+            else:
+                patient.esv_mask = self.transform(patient.esv_mask)
+                patient.edv_mask = self.transform(patient.edv_mask)
+        
+
 
         return {'FileName': patient.patient_id,
             'frames': {'esv': patient.esv_frame,
                    'edv': patient.edv_frame},
-        'masks': {'esv': patient.esv_mask,
-                  'edv': patient.edv_mask}}
+            'masks': {'esv': torch.tensor(patient.esv_mask).float(),
+                      'edv': torch.tensor(patient.edv_mask).float()}}
 
 class FullVideoDataset(Dataset):
-    def __init__(self, root_dir, input_transform=None):
+    def __init__(self, root_dir, transform=None, resize_flag=False):
         self.root_dir = root_dir
         csv_file = os.path.join(root_dir, 'Database', 'training_database.csv')
         self.df = pd.read_csv(csv_file)
-        self.input_transform = input_transform
+        self.transform = transform
+        self.resize_flag = resize_flag
 
     def __len__(self):
         return len(self.df)
@@ -189,10 +206,16 @@ class FullVideoDataset(Dataset):
             cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
             ret, frame = cap.read()
             if ret:
-                if self.input_transform:
-                    frames.append(self.input_transform(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)))
+                if self.transform:
+                    if self.resize_flag:
+                        frames.append(self.transform(cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), (224, 224))))
+                    else:
+                        frames.append(self.transform(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)))
                 else:
-                    frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+                    if self.resize_flag:
+                        frames.append(cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), (224, 224)))
+                    else:
+                        frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
             else:
                 raise Exception(f"Failed to read frame {frame_idx} from video {patient.video_path}")
         cap.release()
@@ -203,3 +226,146 @@ class FullVideoDataset(Dataset):
             frames.append(torch.zeros_like(frames[0]))
 
         return {'patient_id': patient.patient_id, 'frames': torch.stack(frames), 'fps': patient.fps}
+    
+class CardiacVideoDataset(Dataset):
+    def __init__(self, root_dir, input_transform=None, target_transform=None, split_group=None, split_method=None, test_size=0.2, val_size=0.2):
+        self.root_dir = root_dir
+        csv_file = os.path.join(root_dir, 'Database', 'training_database.csv')
+        self.df = pd.read_csv(csv_file)
+        self.df_train = None
+        self.df_val = None
+        self.df_test = None
+        self.input_transform = input_transform
+        self.target_transform = target_transform
+        self.split_group = split_group
+        self.split_method = split_method
+        self.test_size = test_size
+        self.val_size = val_size
+
+        if not self.split_method:
+            self.df_train = self.df[self.df['Split'].str.lower() == 'train']
+            self.df_val = self.df[self.df['Split'].str.lower() == 'val']
+            self.df_test = self.df[self.df['Split'].str.lower() == 'test']
+        elif self.split_method == 'EF':
+            self.df['EF_bins'] = pd.cut(self.df['EF'], bins=10)
+            self.df_train, self.df_test = train_test_split(self.df, test_size=self.test_size, stratify=self.df['EF_bins'], random_state=42)
+            self.df_train, self.df_val = train_test_split(self.df_train, test_size=self.val_size/(1-self.test_size), stratify=self.df_train['EF_bins'], random_state=42)
+
+        if self.split_group == 'train':
+            self.df = self.df_train
+        elif self.split_group == 'val':
+            self.df = self.df_val
+        elif self.split_group == 'test':
+            self.df = self.df_test
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+        patient = Patient(row['FileName'].replace('.avi', ''))
+        patient.video_path = os.path.join(self.root_dir, 'EchoNet-Dynamic', 'Videos', row['FileName'])
+        patient.esv_idx = row['ESV_frame']
+        patient.edv_idx = row['EDV_frame']
+        patient.esv_poly = row['ESV_polygon']
+        patient.edv_poly = row['EDV_polygon']
+
+        cap = cv2.VideoCapture(patient.video_path)
+
+        frames = []
+        masks = []
+
+        for index in [patient.esv_idx, patient.edv_idx]:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, index)
+            ret, frame = cap.read()
+            if ret:
+                gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                if self.input_transform:
+                    gray_frame = self.input_transform(gray_frame)
+                frames.append(gray_frame)
+
+                mask = np.zeros((gray_frame.shape[1], gray_frame.shape[2]), dtype=np.uint8)
+                polygon = convert_to_list(patient.esv_poly if index == patient.esv_idx else patient.edv_poly)
+                cv2.fillPoly(mask, [np.array(polygon)], 255)
+                mask = torch.tensor(mask, dtype=torch.float32).unsqueeze(0)  # Add channel dimension
+                if self.target_transform:
+                    mask = self.target_transform(mask.numpy())  # Apply transform if mask is not already a tensor
+                masks.append(mask)
+
+        cap.release()
+
+        frames = torch.stack(frames)  # Shape: (sequence, channels, height, width)
+        masks = torch.stack(masks)  # Shape: (sequence, channels, height, width)
+
+        return {'FileName': patient.patient_id, 'frames': frames, 'masks': masks}
+
+class CardiacVitDataset(Dataset):
+    def __init__(self, root_dir, transform=None, split_group=None, split_method=None, test_size=0.2, val_size=0.2):
+        self.root_dir = root_dir
+        csv_file = os.path.join(root_dir, 'Database', 'training_database.csv')
+        self.df = pd.read_csv(csv_file)
+        self.df_train = None
+        self.df_val = None
+        self.df_test = None
+        self.transform = transform
+        self.split_group = split_group
+        self.split_method = split_method
+        self.test_size = test_size
+        self.val_size = val_size
+
+        if not self.split_method:
+            self.df_train = self.df[self.df['Split'].str.lower() == 'train']
+            self.df_val = self.df[self.df['Split'].str.lower() == 'val']
+            self.df_test = self.df[self.df['Split'].str.lower() == 'test']
+        elif self.split_method == 'EF':
+            self.df['EF_bins'] = pd.cut(self.df['EF'], bins=10)
+            self.df_train, self.df_test = train_test_split(self.df, test_size=self.test_size, stratify=self.df['EF_bins'], random_state=42)
+            self.df_train, self.df_val = train_test_split(self.df_train, test_size=self.val_size/(1-self.test_size), stratify=self.df_train['EF_bins'], random_state=42)
+
+        if self.split_group == 'train':
+            self.df = self.df_train
+        elif self.split_group == 'val':
+            self.df = self.df_val
+        elif self.split_group == 'test':
+            self.df = self.df_test
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+        patient_id = row['FileName'].replace('.avi', '')
+        video_path = os.path.join(self.root_dir, 'EchoNet-Dynamic', 'Videos', row['FileName'])
+        esv_idx = row['ESV_frame']
+        edv_idx = row['EDV_frame']
+        esv_poly = row['ESV_polygon']
+        edv_poly = row['EDV_polygon']
+
+        cap = cv2.VideoCapture(video_path)
+
+        frames = []
+        masks = []
+
+        for index in [esv_idx, edv_idx]:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, index)
+            ret, frame = cap.read()
+            if ret:
+                gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                if self.input_transform:
+                    gray_frame = self.input_transform(gray_frame)
+                frames.append(gray_frame)
+
+                mask = np.zeros((gray_frame.shape[0], gray_frame.shape[1]), dtype=np.uint8)
+                polygon = convert_to_list(esv_poly if index == esv_idx else edv_poly)
+                cv2.fillPoly(mask, [np.array(polygon)], 255)
+                mask = torch.tensor(mask, dtype=torch.float32).unsqueeze(0)  # Add channel dimension
+                if self.target_transform:
+                    mask = self.target_transform(mask.numpy())  # Apply transform if mask is not already a tensor
+                masks.append(mask)
+
+        cap.release()
+
+        frames = torch.stack(frames)  # Shape: (sequence, channels, height, width)
+        masks = torch.stack(masks)  # Shape: (sequence, channels, height, width)
+
+        return {'patient_id': patient_id, 'frames': frames, 'masks': masks}
